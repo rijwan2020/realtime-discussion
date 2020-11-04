4ef76c97e49a604fbe5dd83523167ddeefbccfc8
Rijwan <rijwan@4visionmedia,com>
2020-11-04 21:29:40 +0700

    Create relationships table

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace App\Models\Model;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Question extends Model
+class Category extends Model
 {
     use HasFactory;
 }
